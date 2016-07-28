1784dbdffd0755bc9389e9643cfcae69df7022f6
rexxies <r.s.wijnaldum@live.nl>
2016-07-28 15:31:58 +0200

    HangmanServlet.java 2e revision
    added a player bean
    program now plays continually

--- Hangman/HangmanWeb/HangmanServlet.java
+++ Hangman/HangmanWeb/HangmanServlet.java
@@ -1,137 +1,279 @@
-package nl.didacticum.servlets;
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<html>
+	
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Hangman</title>
+</head>
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-
-/**
- * Servlet implementation class Hangman
- */
-@WebServlet("/HangmanServlet")
-public class HangmanServlet extends HttpServlet {
+<body>
+	<h1>Hangman</h1>
+	<!-- TR = table row, TD is tabel colum -->
 	
 	
-	public HangmanServlet() {
-        super();
-        //empty constructor
-    }
+<FORM action="/HangmanWeb/HangmanServlet" method="post">
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
+<c:if test="${player.getName() == null}">
+	<H4>Insert name</H4>
+	<INPUT type="text" name="userName">
+</c:if>
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	<TABLE width="50%" border="0" cellpadding="0" cellspacing="1">
+	<TBODY>
+<!-- New row -->	
+		<TR>
+			<TD>
+				Enter a letter
+			</TD>
 		
-		HttpSession session = request.getSession();
-		
-		
-		String[] answer = (String[])session.getAttribute("answer");
-		String hangman = "  __\n |  |\n |  o\n | \\|/ \n | / \\ \n_|_";	
-		
-
-		// if session is not set pick a new word
-		if(answer == null){
-			String line;
-			String[] wordToGuess={". ",". ",". ",". ",". ",". "}, groeneBoekje = null; 
-			ArrayList<String> sixLetterWords = new ArrayList();
-			boolean win=false, guessWasRight = false, lose = false;;
-			int timesGuessedWrong=0;
+			<TD>
+				Solution
+			</TD>
 			
-			HashMap<String, Integer> alreadyChosenLetters = new HashMap<>();
+			<TD>
+				Chosen Letters
+			</TD>
+		</TR>
+<!-- New row -->		
+		<TR>
+			<TD><!-- textfield for user input -->
+				<INPUT type="text" name="userGuess"> <!-- Input field for the user's guess, name moet zelfde zijn als String bij doPost() -->
+			</TD>
 			
-			URL boekjeUrl = new URL("http://unconnected.nl/tmp/groene_boekje.txt"); 
-			BufferedReader in = new BufferedReader(new InputStreamReader(boekjeUrl.openStream()));
-		
-			while ((line = in.readLine()) != null ) {
-				groeneBoekje = line.split(" ");
-				for(int i = 0; i<groeneBoekje.length; i++){
-					if(groeneBoekje[i].length() == 6){
-						sixLetterWords.add(groeneBoekje[i]);
-					}//end if
-				}//end for
-			}//end while	
+			<!-- the word with the dots-->	
+			<TD>
+			<c:forEach var="wordToGuess" items="${wordToGuess}">
 			
-			int random = new Random().nextInt(sixLetterWords.size());
-			String tmp = sixLetterWords.get(random);
-			answer = tmp.toLowerCase().split("");	//this is the complete word that has to be guessed
-			
-			System.out.println( answer[0] + answer[1] + answer[2] + answer[3] + answer[4] + answer[5]);
-			//set answer in session scope 
-			session.setAttribute("answer", answer); 
-			session.setAttribute("wordToGuess", wordToGuess);
-			session.setAttribute("guessWasRight", guessWasRight);
-			session.setAttribute("timesGuessedWrong", timesGuessedWrong);
-			session.setAttribute("hangman", hangman);
-			session.setAttribute("alreadyChosenLetters", alreadyChosenLetters);
-			session.setAttribute("win", win);
-			session.setAttribute("lose", lose);
-		}
+  	  			<c:out value="${wordToGuess}"/>
 		
-		//else use the word that is saved in session. (remember to use cast!!! session saves a object
-		String[] wordToGuess = (String[])session.getAttribute("wordToGuess");
-		boolean guessWasRight = (boolean)session.getAttribute("guessWasRight"); 
-		boolean win = (boolean)session.getAttribute("win");
-		boolean lose = (boolean)session.getAttribute("lose");
-		int timesGuessedWrong = (Integer)session.getAttribute("timesGuessedWrong");
-		HashMap alreadyChosenLetters =(HashMap)session.getAttribute("alreadyChosenLetters"); 
+			</c:forEach>
+			</TD>
+				
+			<TD>
+				<c:forEach var= "key" items="${alreadyChosenLetters}">
+					<c:out value="${key.key}"/>
+				</c:forEach>
+			</TD>
+		</TR>
+<!-- New row -->		
+		<TR>
+			<TD><!-- submit button -->
+				<INPUT type="submit" value="Guess">
+			</TD> 
+		</TR>
+<!-- New row -->		
+		<TR>
+			<TD>
+			<P> Je mag nog 
+				<c:out value="${10 - timesGuessedWrong }"/>
+				 keer raden
+			</P>
+			</TD>
+		</TR>
+<!-- New row -->		
+		<TR>
+			<TD>
+			<c:choose>
+				<c:when test="${timesGuessedWrong == 10 }">
+					<h2> You Lose!!!!</h2>
+					Het antwoord was:   
+					<c:forEach var="answer" items="${answer}">
+  	  					<c:out value="${answer}"/>
+					</c:forEach>
+					<TD><!-- submit button -->
+						<INPUT type="submit" value="Try Again">
+					</TD>
+				</c:when>
+	
+				<c:when test="${win==true || win ==null && player.getName() !=null}">
+					<h2> You Win!!!!</h2>
+					<TD><!-- submit button -->
+					<H3> Fill in your name</H3>
+						<INPUT type="text" name="userName">
+						<INPUT type="submit"  value="try again" >
+    				</TD>
+				</c:when>
+			</c:choose>
+			</TD>
+		</TR>
 		
-		if(timesGuessedWrong ==10){
-			lose=true;
-		}
-		
-		if(win==true|| lose == true){
-			session.invalidate();
-			request.getRequestDispatcher("/index.jsp").forward(request, response);
-		}
-		
-		// user gives a word/letter to be matched
-		String userGuess = request.getParameter("userGuess");
+	</TBODY>
+	</TABLE>
+</FORM>
 
-		//check if guess is correct
-		for(int i = 0; i<answer.length; i++){
-			if(userGuess.equals(answer[i])){
-				wordToGuess[i]=userGuess;
-				alreadyChosenLetters.put(userGuess, 1);
-				guessWasRight = true;
-			}//end if
-		}//end for
-		
-		 if(guessWasRight==false){
-				alreadyChosenLetters.put(userGuess, 1);
-				timesGuessedWrong++;				
-			}//end if
-		
-		System.out.println("user entered: "+ userGuess + " " + answer[0] + answer[1] + answer[2] + answer[3] + answer[4] + answer[5]);
-		System.out.println(wordToGuess[0]+ wordToGuess[1]+ wordToGuess[2]+ wordToGuess[3]+ wordToGuess[4]+ wordToGuess[5]);
-		
-		 if(answer[0].equals(wordToGuess[0]) && answer[1].equals(wordToGuess[1]) && answer[2].equals(wordToGuess[2]) && 
-					answer[3].equals(wordToGuess[3]) && answer[4].equals(wordToGuess[4]) && answer[5].equals(wordToGuess[5])){
-				win = true;
-			}
-
-		guessWasRight = false;
-		session.setAttribute("guessWasRight", guessWasRight);
-		session.setAttribute("alreadyChosenLetters", alreadyChosenLetters);
-		session.setAttribute("timesGuessedWrong", timesGuessedWrong);
-		session.setAttribute("win", win);
-		session.setAttribute("lose", lose);
-		
-		
-		request.getRequestDispatcher("/index.jsp").forward(request, response);
-
-	}
-
+<canvas id="hangmanCanvas"></canvas>
+<script>
+var frame1 = function(){
+	var canvas = document.getElementById('hangmanCanvas');
+	var context = canvas.getContext('2d');
+	
+      context.beginPath();
+      context.moveTo(0, 150);
+      context.lineTo(150, 150);
+      context.stroke();
 }
+var frame2 = function() {
+	var canvas = document.getElementById('hangmanCanvas');
+	var context = canvas.getContext('2d');
+	
+      context.beginPath();
+      context.moveTo(10, 0);
+      context.lineTo(10, 600);
+      context.stroke();
+}
+var frame3 = function() {
+	var canvas = document.getElementById('hangmanCanvas');
+	var context = canvas.getContext('2d');
+	
+      context.beginPath();
+      context.moveTo(0, 5);
+      context.lineTo(70, 5);
+      context.stroke();
+}
+var frame4 = function() {
+	var canvas = document.getElementById('hangmanCanvas');
+	var context = canvas.getContext('2d');
+	
+      context.beginPath();
+      context.moveTo(60, 5);
+      context.lineTo(60, 15);
+      context.stroke();
+}
+var torso = function() {
+	var canvas = document.getElementById('hangmanCanvas');
+	var context = canvas.getContext('2d');
+	
+      context.beginPath();
+      context.moveTo(60, 36);
+      context.lineTo(60, 70);
+      context.stroke();
+}
+var rightArm = function() {
+	var canvas = document.getElementById('hangmanCanvas');
+	var context = canvas.getContext('2d');
+	
+      context.beginPath();
+      context.moveTo(60, 46);
+      context.lineTo(100, 50);
+      context.stroke();
+}
+var leftArm = function() {
+	var canvas = document.getElementById('hangmanCanvas');
+	var context = canvas.getContext('2d');
+	
+      context.beginPath();
+      context.moveTo(60, 46);
+      context.lineTo(20, 50);
+      context.stroke();
+}
+var rightLeg = function() {
+	var canvas = document.getElementById('hangmanCanvas');
+	var context = canvas.getContext('2d');
+	
+      context.beginPath();
+      context.moveTo(60, 70);
+      context.lineTo(100, 100);
+      context.stroke();
+}
+var leftLeg = function() {
+	var canvas = document.getElementById('hangmanCanvas');
+	var context = canvas.getContext('2d');
+	
+      context.beginPath();
+      context.moveTo(60, 70);
+      context.lineTo(20, 100);
+      context.stroke();
+}
+var head = function() {
+	var canvas = document.getElementById('hangmanCanvas');
+	var context = canvas.getContext('2d');
+    context.beginPath();
+    context.arc(60, 25, 10, 0, Math.PI*2, true);
+    context.stroke();
+}
+</script>
+
+<c:choose>
+	<c:when test="${timesGuessedWrong == 1}">
+		<script>frame1();</script>
+	</c:when>
+	<c:when test="${timesGuessedWrong == 2}">
+		<script>frame1();</script>
+		<script>frame2();</script>
+	</c:when>
+	<c:when test="${timesGuessedWrong == 3}">
+		<script>frame1();</script>
+		<script>frame2();</script>
+		<script>frame3();</script>
+	</c:when>
+	<c:when test="${timesGuessedWrong == 4}">
+		<script>frame1();</script>
+		<script>frame2();</script>
+		<script>frame3();</script>
+		<script>frame4();</script>
+	</c:when>
+	<c:when test="${timesGuessedWrong == 5}">
+		<script>frame1();</script>
+		<script>frame2();</script>
+		<script>frame3();</script>
+		<script>frame4();</script>
+		<script>head();</script>
+	</c:when>
+	<c:when test="${timesGuessedWrong == 6}">
+		<script>frame1();</script>
+		<script>frame2();</script>
+		<script>frame3();</script>
+		<script>frame4();</script>
+		<script>head();</script>
+		<script>torso();</script>
+	</c:when>
+	<c:when test="${timesGuessedWrong == 7}">
+		<script>frame1();</script>
+		<script>frame2();</script>
+		<script>frame3();</script>
+		<script>frame4();</script>
+		<script>head();</script>
+		<script>torso();</script>
+		<script>leftArm();</script>
+	</c:when>
+	<c:when test="${timesGuessedWrong == 8}">
+		<script>frame1();</script>
+		<script>frame2();</script>
+		<script>frame3();</script>
+		<script>frame4();</script>
+		<script>head();</script>
+		<script>torso();</script>
+		<script>leftArm();</script>
+		<script>rightArm();</script>
+	</c:when>
+	<c:when test="${timesGuessedWrong == 9}">
+		<script>frame1();</script>
+		<script>frame2();</script>
+		<script>frame3();</script>
+		<script>frame4();</script>
+		<script>head();</script>
+		<script>torso();</script>
+		<script>leftArm();</script>
+		<script>rightArm();</script>
+		<script>leftLeg();</script>
+	</c:when>
+	<c:when test="${timesGuessedWrong == 10}">
+		<script>frame1();</script>
+		<script>frame2();</script>
+		<script>frame3();</script>
+		<script>frame4();</script>
+		<script>head();</script>
+		<script>torso();</script>
+		<script>leftArm();</script>
+		<script>rightArm();</script>
+		<script>leftLeg();</script>
+		<script>rightLeg();</script>
+	</c:when>
+</c:choose>
+
+</body>
+</html>
